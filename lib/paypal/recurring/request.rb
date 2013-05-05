ea116a410d1e5c9120f7014f3e33e63fe9b58df7
Xin Fan <fx83060856@gmail.com>
2013-05-06 01:53:13 +0930

    add X-PP-AUTHORIZATION header

--- lib/paypal/recurring/request.rb
+++ lib/paypal/recurring/request.rb
@@ -124,8 +124,6 @@ module PayPal
           http["X-PAYPAL-APPLICATION-ID"] = PayPal::Recurring.application_id
           http["X-PAYPAL-SECURITY-SIGNATURE"] = PayPal::Recurring.signature
           http["X-PP-AUTHORIZATION"] = PayPal::Recurring.authorization
-          http["X-PAYPAL-REQUEST-DATA-FORMAT"] = "JSON"
-          http["X-PAYPAL-RESPONSE-DATA-FORMAT"] = "JSON"
         end
       end
 
